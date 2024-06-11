cd1c4bc18bd0456cf7b531625f2403f06ee45a02
Ryan Clements <ryanclementshax@gmail.com>
2024-06-10 20:43:43 -0400

    use auto imports on server side

--- talks/2024-06-13/src/examples/nuxt-project/server/routes/final/notes.get.ts
+++ talks/2024-06-13/src/examples/nuxt-project/server/routes/final/notes.get.ts
@@ -1,6 +1,3 @@
-import { getNotes } from '../../db/notes'
-import { defineEventHandler } from 'h3'
-
 export default defineEventHandler(async () => {
   return getNotes()
 })
--- talks/2024-06-13/src/examples/nuxt-project/server/routes/final/notes/[id].delete.ts
+++ talks/2024-06-13/src/examples/nuxt-project/server/routes/final/notes/[id].delete.ts
@@ -1,6 +1,3 @@
-import { deleteNote } from '../../../db/notes'
-import { defineEventHandler, getRouterParam } from 'h3'
-
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id')
   deleteNote(Number(id))
